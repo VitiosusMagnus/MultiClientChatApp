d2253efcb7edb4b92c127362cfc9b6fcac4622aa
VitiosusMagnus <furkancanokal1@gmail.com>
2023-09-18 19:26:38 +0300

    message send and read functionallity added

--- src/server/ClientHandler.java
+++ src/server/ClientHandler.java
@@ -1,4 +1,40 @@
 package server;
 
-public class ClientHandler {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class ClientHandler implements Runnable{
+    private Socket clientSocket;
+    private PrintWriter writer;
+
+
+    public ClientHandler(Socket clientSocket){
+        this.clientSocket = clientSocket;
+        try {
+            writer = new PrintWriter(clientSocket.getOutputStream(),true);
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+    @Override
+    public void run() {
+        try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            String message;
+            while ((message = reader.readLine()) != null){
+                broadcastMessage(message);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void broadcastMessage(String message){
+        for (Socket socket : ChatServer.clientSockets) {
+            writer.println(message);
+        }
+    }
 }
